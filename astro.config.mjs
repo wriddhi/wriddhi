a6cf5a49907ecfd1e063ac3fc0dc6cad55b59b64
wriddhi <wriddhihazra@gmail.com>
2024-11-03 22:28:17 +0530

    added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
-
+import vercel from '@astrojs/vercel/serverless';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react(), sitemap(), icon()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+  integrations: [tailwind(), react(), sitemap(), icon()],
 });
